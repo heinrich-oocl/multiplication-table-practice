c3fc84c673916ddf3021934b0b0e0759307ccebc
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-11-29 15:34:51 +0800

    init

---
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -0,0 +1,40 @@
+package com.tw;
+
+public class MultiplicationTable {
+    public String create(int start, int end) {
+        return isValid(start, end) ? generateTable(start, end) : null;
+    }
+
+    public Boolean isValid(int start, int end) {
+        return isInRange(start) && isStartNotBiggerThanEnd(start, end);
+    }
+
+    public Boolean isInRange(int number) {
+        return number<=1000 && number>=1;
+    }
+
+    public Boolean isStartNotBiggerThanEnd(int start, int end) {
+        return end >= start;
+    }
+
+    public String generateTable(int start, int end) {
+        StringBuilder line = new StringBuilder();
+        for (int i=start; i<=end; i++){
+            line.append(generateLine(start, i));
+            line.append(System.lineSeparator());
+        }
+        return line.toString().trim();
+    }
+
+    public String generateLine(int start, int row) {
+        StringBuilder line = new StringBuilder();
+        for (int i=start; i<=row; i++){
+            line.append(generateSingleExpression(i, row)).append("  ");
+        }
+        return line.toString().trim();
+    }
+
+    public String generateSingleExpression(int multiplicand, int multiplier) {
+        return String.format("%d*%d=%d", multiplicand, multiplier, multiplicand*multiplier);
+    }
+}
